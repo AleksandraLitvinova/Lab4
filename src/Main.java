eed30a0f84edc8a33065f1cc989e02d734621da9
Alex <alya.brosko@br.ru>
2019-01-20 13:10:53 +0300

    Создан новый проект

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String way = reader.readLine();
+        System.out.println("Введите путь к файлу, в который будет скопировано содержимое.");
+        BufferedReader reader2 = new BufferedReader(new InputStreamReader(System.in));
+        String way2 = reader2.readLine();
+        File fileWork;
+        try {
+            fileWork = new File();
+            System.out.println("Содержимое успешно скопировано со скоростью прогресса " + fileWork.copy(way, way2) + " c");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
